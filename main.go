a716ab8b8c5bdd46a25099e2d7dc9fe03cdf8d88
Duong <duongplus1011@gmail.com>
2022-09-05 14:23:20 +0700

    first

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	dsn := os.Getenv("DBConnStr")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("Cannot connect to MySQL:", err)
+	}
+
+	log.Println("Connected:", db)
+
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+	{
+		restaurants := v1.Group("restaurants")
+		restaurants.GET("", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"status": "ok",
+			})
+		})
+	}
+
+	if err := router.Run(); err != nil {
+
+	}
+}
+
+type Restaurant struct {
+	Id      int    `json:"id" gorm:"column:id;"`
+	Name    string `json:"name" gorm:"column:name;"`
+	Address string `json:"address" gorm:"column:addr"`
+}
+
+func (Restaurant) TableName() string { return "restaurants" }
+
+type RestaurantCreate struct {
+	Name    string `json:"name" gorm:"column:name;"`
+	Address string `json:"address" gorm:"column:addr;"`
+}
+
+func (RestaurantCreate) TableName() string { return Restaurant{}.TableName() }
+
+type RestaurantUpdate struct {
+	Name    *string `json:"name" gorm:"column:name;"`
+	Address *string `json:"address" gorm:"column:addr;"`
+}
+
+func (RestaurantUpdate) TableName() string { return Restaurant{}.TableName() }
